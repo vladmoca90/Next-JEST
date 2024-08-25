ed99b4fde6b7f648074e982651b154e56f3917db
Vlad Mocanu <vladmoca90@gmail.com>
2024-08-25 02:50:58 +0100

    Cleaned the lines betwen the file code

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,23 @@
-export const metadata = {
-  title: "App Router",
-};
+"use client";
+import { useState } from "react";
 
-export default function Page() {
-  return <h1>App Router</h1>;
+export default function Counter() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h2>{count}</h2>
+      <button type="button" onClick={() => setCount(count + 1)}>
+        +
+      </button>
+    </>
+  );
 }
+
+// export const metadata = {
+//   title: "App Router",
+// };
+
+// export default function Page() {
+//   return <h1>App Router</h1>;
+// }
